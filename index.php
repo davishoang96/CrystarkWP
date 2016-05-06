ad259596f868d26966220762e0779776007fa75b
Davis Hoang <davishoang96@gmail.com>
2016-05-06 21:06:37 +0700

    Update 2.1.3 - Modified Front Page

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <?php
 
     ?>
-    <div class="container" id="coverpage" data-stellar-background-ratio="0.4">
-      <div class="col-lg-12">
+    <div class="container" id="coverpage" data-stellar-background-ratio="0.8">
+
         <div class="row">
           <div class="text-center">
             <h1> Howdy everyone! this is my <?php  bloginfo('name'); ?></h1>
@@ -18,38 +18,41 @@
             <button href="<?php the_permalink('about');?>" class="btn btn-success" type="button" name="button">Contact</button>
           </div>
         </div>
-      </div>
+
     </div>
 
-
     <?php
-
-    if(have_posts()){
-      while (have_posts()) {
-        the_post();
-        ?>
-        <article class="the-post">
+      if (have_posts()) {
+        while (have_posts()) {
+          the_post();
+          ?>
           <div class="container">
             <div class="row">
-              <div class="col-md-3">
-                <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-                <div class="thumbnail">
-                  <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(300, 200)); ?></a>
-                </div>
-                <p><?php the_excerpt() ?></p>
-                <label><?php the_time(); ?> | by <?php the_author(); ?></label>
+              <div class="col-md-4">
+                <h3><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h3>
+                <?php the_post_thumbnail(array(300,200)) ?>
+                <p>
+                  <?php the_excerpt(); ?>
+                </p>
+                <label for=""><?php the_time() ?>| by <?php the_author(); ?></label>
               </div>
             </div>
           </div>
-        </article>
-        <?php
+          <?php
+        }
+      } else {
+        echo "No post";
       }
-    } else {
-      ?>
-      <h1>NO CONTENT</h1>
-      <?php
-    }
+
+
+
+
+
+
+
     ?>
+
+
 
 
 

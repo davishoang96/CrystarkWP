a16d1949d99b99484212ab81e6530430d1e56d82
Davis Hoang <davishoang96@gmail.com>
2016-05-07 22:57:51 +0700

    Update 2.2 - New Index Page

--- index.php
+++ index.php
@@ -1,59 +1,44 @@
 <?php get_header();
 
-    ?>
-    <div id='cssmenu'>
-      <?php wp_nav_menu(); ?>
-    </div>
-    <?php
+?>
+<div id='cssmenu'>
+	<?php wp_nav_menu(); ?>
+</div>
+<?php
 
-    ?>
-    <div class="container" id="coverpage" data-stellar-background-ratio="0.8">
+?>
 
-        <div class="row">
-          <div class="text-center">
-            <h1> Howdy everyone! this is my <?php  bloginfo('name'); ?></h1>
-            <p>
-              Sign up and Join us
-            </p>
-            <button href="<?php the_permalink('about');?>" class="btn btn-success" type="button" name="button">Contact</button>
-          </div>
-        </div>
+		<?php if (have_posts()): while(have_posts()) : the_post(); ?>
 
-    </div>
 
-    <?php
-      if (have_posts()) {
-        while (have_posts()) {
-          the_post();
-          ?>
-          <div class="container">
-            <div class="row">
-              <div class="col-md-4">
-                <h3><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h3>
-                <?php the_post_thumbnail(array(300,200)) ?>
-                <p>
-                  <?php the_excerpt(); ?>
-                </p>
-                <label for=""><?php the_time() ?>| by <?php the_author(); ?></label>
-              </div>
-            </div>
-          </div>
-          <?php
-        }
-      } else {
-        echo "No post";
-      }
+			<div class="container-fluid" id="conter">
+				<div class="row">
+					<div class="col-md-8">
+						<div class="wrapper">
+							<div class="text-center">
+								<div class="thumbnail">
+									<?php the_post_thumbnail('medium'); ?>
+								</div>
+							</div>
+							<div class="posts">
+								<h3><a href="<?php the_permalink()?>"><?php the_title(); ?></a></h3>
+								<label><?php the_time()?> | by <?php the_author(); ?></label>
+								<p>
+									<?php the_excerpt(); ?>
+								</p>
+								<a href="<?php the_permalink()?>" class="view-more">View more</a>
+								<hr>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		<?php endwhile; ?>
+		<?php endif; ?>
 
 
 
 
 
-
-
-    ?>
-
-
-
-
-
-    <?php get_footer(); ?>
+<?php get_footer(); ?>

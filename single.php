c72711d94089b9679c8d092e05d50864a1aa137d
Davis Hoang <davishoang96@gmail.com>
2016-05-08 16:38:11 +0700

    Update 2.2.1 - Added cover image in single page

--- single.php
+++ single.php
@@ -11,11 +11,15 @@
 
     the_post();
     ?>
-    <div class="container">
+    <div class="background-news" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID) , 'max-control' ); ?>');">
+      <div class="title-news">
+        <h1><?php the_title(); ?></h1>
+      </div>
+    </div>
+
+    <div class="container news-posts">
       <div class="row">
-        <div class="col-md-12">
-          <h1><?php the_title(); ?></h1>
-          <?php the_post_thumbnail('medium') ?>
+        <div class="col-lg-8">
           <p>
             <?php the_content(); ?>
           </p>

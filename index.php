ac64a4d2424b81aae4b1126e64efd7e36cda0657
Davis Hoang <davishoang96@gmail.com>
2016-05-10 11:25:00 +0700

    Update 2.2.3 - Reset all

--- index.php
+++ index.php
@@ -1,44 +1,19 @@
-<?php get_header();
-
-?>
-<div id='cssmenu'>
-	<?php wp_nav_menu(); ?>
-</div>
 <?php
+/**
+ * The main template file.
+ * @package metro-creativex
+ */
+get_header(); ?>
+		<div id="cssmenu">
+			<?php wp_nav_menu(); ?>
+		</div>
 
-?>
-
-		<?php if (have_posts()): while(have_posts()) : the_post(); ?>
-
-
-			<div class="container-fluid" id="conter">
-				<div class="row">
-					<div class="col-md-8">
-						<div class="wrapper">
-							<div class="text-center">
-								<div class="thumbnail">
-									<?php the_post_thumbnail('medium'); ?>
-								</div>
-							</div>
-							<div class="posts">
-								<h3><a href="<?php the_permalink()?>"><?php the_title(); ?></a></h3>
-								<label><?php the_time()?> | by <?php the_author(); ?></label>
-								<p>
-									<?php the_excerpt(); ?>
-								</p>
-								<a href="<?php the_permalink()?>" class="view-more">View more</a>
-								<hr>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-
-		<?php endwhile; ?>
+		<div class="container">
+			<?php if ( have_posts() ) : ?>
+			<?php /* The loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php endwhile; ?>
 		<?php endif; ?>
-
-
-
-
-
+		</div><!-- /content -->
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,45 +1,41 @@
+
 <?php
-  get_header();
-
+get_header();
+?>
+<div id='cssmenu'>
+  <?php wp_nav_menu(); ?>
+</div>
+<?php
+if(have_posts() ) :
+  the_post();
   ?>
-  <div id='cssmenu'>
-    <?php wp_nav_menu(); ?>
+  <div class="background-news" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID) , 'max-control' ); ?>');">
+    <div class="title-news">
+      <h1><?php the_title(); ?></h1>
+    </div>
   </div>
+
+  <div class="container news-posts">
+    <div class="row">
+      <div class="col-lg-8">
+        <p>
+          <?php the_content(); ?>
+        </p>
+      </div>
+    </div>
+  </div>
+
   <?php
-
-  if(have_posts() ) :
-
-    the_post();
+  else :
     ?>
-    <div class="background-news" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID) , 'max-control' ); ?>');">
-      <div class="title-news">
-        <h1><?php the_title(); ?></h1>
-      </div>
-    </div>
-
-    <div class="container news-posts">
+    <div class="container">
       <div class="row">
-        <div class="col-lg-8">
-          <p>
-            <?php the_content(); ?>
-          </p>
+        <div class="col-md-12">
+          <h1 style="color: red">There aren't any post here</h1>
         </div>
       </div>
     </div>
-
     <?php
-
-    else :
-      ?>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <h1 style="color: red">There aren't any post here</h1>
-          </div>
-        </div>
-      </div>
-      <?php
-    endif;
-
+  endif;
   get_footer();
- ?>
+  ?>
